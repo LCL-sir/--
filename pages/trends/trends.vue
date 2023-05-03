eb2b19d22a8ece08b0169ede8c7ce90f6ff57410
liuchenglong <1916770979@qq.com>
2023-05-03 22:52:01 +0800

    日历 未完成

--- pages/trends/trends.vue
+++ pages/trends/trends.vue
@@ -1,28 +1,50 @@
 <template>
 	<view class="container">
-		
+		<view class="top">5月2023</view>
+		<view class="main">
+			<view class="main_week">
+				<span>S</span>
+				<span>M</span>
+				<span>T</span>
+				<span>W</span>
+				<span>T</span>
+				<span>F</span>
+				<span>S</span>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		onLoad(options) {
-			
-		},
-		onShow() {
-			
-		},
-		methods: {
-			
-		},
-	}
+export default {
+	data() {
+		return {
+
+		}
+	},
+	onLoad(options) {
+
+	},
+	onShow() {
+
+	},
+	methods: {
+
+	},
+}
 </script>
 
 <style lang="scss" scoped>
-	
+.container {
+	height: 85.8vh;
+	background-color: #01040D;
+	border-top: 1px solid #01040D;
+}
+
+.container .top {
+	color: #FFFFFF;
+	font-size: 44rpx;
+	margin-top: 40rpx;
+	margin-left: 40rpx;
+}
 </style>
