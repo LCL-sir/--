11d216506e34587de36bb3d05d02787ac1ef9ee8
liuchenglong <1916770979@qq.com>
2023-05-04 23:15:36 +0800

    日历未完成

--- pages/trends/trends.vue
+++ pages/trends/trends.vue
@@ -11,6 +11,9 @@
 				<span>F</span>
 				<span>S</span>
 			</view>
+			<view class="main_m">
+				<view v-for="(item, index) in piqi" :key="index" @click="onrili(item)">{{ item }}</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -19,7 +22,7 @@
 export default {
 	data() {
 		return {
-
+			piqi: [30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 1, 2, 3]
 		}
 	},
 	onLoad(options) {
@@ -29,7 +32,9 @@ export default {
 
 	},
 	methods: {
-
+		onrili(e){
+			console.log(e);
+		}
 	},
 }
 </script>
@@ -39,6 +44,7 @@ export default {
 	height: 85.8vh;
 	background-color: #01040D;
 	border-top: 1px solid #01040D;
+	color: #FFFFFF;
 }
 
 .container .top {
@@ -46,5 +52,25 @@ export default {
 	font-size: 44rpx;
 	margin-top: 40rpx;
 	margin-left: 40rpx;
+}
+
+.main .main_week {
+	display: flex;
+	justify-content: space-around;
+	margin-top: 12px;
+	color: #75818F;
+  margin-bottom: 14px;
+}
+
+.main .main_m {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-around;
+}
+
+.main .main_m view {
+	width: 14%;
+	text-align: center;
+	height: 30px;
 }
 </style>
