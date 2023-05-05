ade01089d0b05921211a4db70bf025456f0255ef
liuchenglong <1916770979@qq.com>
2023-05-05 22:42:24 +0800

    搜索页面

--- pages/Search/Search.vue
+++ pages/Search/Search.vue
@@ -1,48 +1,61 @@
 <template>
 	<view class="container">
 		<view class="Search">
-			<img src="../../static/image/index/Search.png" alt="">
-			<input type="text" />
+			<img src="../../static/image/index/zsou.png" alt="">
+			<div class="shuru">
+				<input type="text" placeholder="搜索课程" />
+			</div>
 			<view class="xunlian">
 				推荐
+			</view>
+			<view>
+				<img src="../../static/image/index/datu.png" alt="">
 			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
+export default {
+	data() {
+		return {
 
-			}
-		},
-		onLoad(options) {
+		}
+	},
+	onLoad(options) {
 
-		},
-		onShow() {
+	},
+	onShow() {
 
-		},
-		methods: {
+	},
+	methods: {
 
-		},
-	}
+	},
+}
 </script>
 
 <style lang="scss" scoped>
-	.container {
-		padding: 0 40rpx;
-	}
+.container {
+	padding: 0 40rpx;
+}
 
-	.container .Search {
-		margin-top: 40rpx;
-	}
+.container .Search {
+	margin-top: 40rpx;
+}
 
-	.container .Search input {
-		z-index: 1;
-	}
-	.container .xunlian {
-		color: #334154;
-		margin-top: 20rpx;
-	}
+.container .Search input {
+	z-index: 1;
+}
+
+.container .xunlian {
+	color: #334154;
+	margin-top: 20rpx;
+	margin-bottom: 20rpx;
+}
+
+.container .shuru {
+	position: relative;
+	top: -68rpx;
+	left: 114rpx;
+}
 </style>
